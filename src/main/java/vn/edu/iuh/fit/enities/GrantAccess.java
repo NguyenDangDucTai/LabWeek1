b58094bd1a32c9058fdf8cc5874189c0d0116295
Student <matyga201013@gmail.com>
2023-09-13 19:42:30 +0700

    Commit lan dau

--- src/main/java/vn/edu/iuh/fit/enities/GrantAccess.java
+++ src/main/java/vn/edu/iuh/fit/enities/GrantAccess.java
@@ -1,4 +1,49 @@
 package vn.edu.iuh.fit.enities;
 
 public class GrantAccess {
+    String role_id;
+    int is_grant;
+    String note;
+
+    public GrantAccess() {
+    }
+
+    public GrantAccess(String role_id, int is_grant, String note) {
+        this.role_id = role_id;
+        this.is_grant = is_grant;
+        this.note = note;
+    }
+
+    public String getRole_id() {
+        return role_id;
+    }
+
+    public void setRole_id(String role_id) {
+        this.role_id = role_id;
+    }
+
+    public int getIs_grant() {
+        return is_grant;
+    }
+
+    public void setIs_grant(int is_grant) {
+        this.is_grant = is_grant;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "GrantAccess{" +
+                "role_id='" + role_id + '\'' +
+                ", is_grant=" + is_grant +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
--- src/main/java/vn/edu/iuh/fit/enities/Log.java
+++ src/main/java/vn/edu/iuh/fit/enities/Log.java
@@ -1,4 +1,62 @@
 package vn.edu.iuh.fit.enities;
 
+import java.sql.Date;
+
 public class Log {
+    String log_id;
+    Date login_time;
+    Date logout_time;
+    String note;
+
+    public Log() {
+    }
+
+    public Log(String log_id, Date login_time, Date logout_time, String note) {
+        this.log_id = log_id;
+        this.login_time = login_time;
+        this.logout_time = logout_time;
+        this.note = note;
+    }
+
+    public String getLog_id() {
+        return log_id;
+    }
+
+    public void setLog_id(String log_id) {
+        this.log_id = log_id;
+    }
+
+    public Date getLogin_time() {
+        return login_time;
+    }
+
+    public void setLogin_time(Date login_time) {
+        this.login_time = login_time;
+    }
+
+    public Date getLogout_time() {
+        return logout_time;
+    }
+
+    public void setLogout_time(Date logout_time) {
+        this.logout_time = logout_time;
+    }
+
+    public String getNote() {
+        return note;
+    }
+
+    public void setNote(String note) {
+        this.note = note;
+    }
+
+    @Override
+    public String toString() {
+        return "Log{" +
+                "log_id='" + log_id + '\'' +
+                ", login_time=" + login_time +
+                ", logout_time=" + logout_time +
+                ", note='" + note + '\'' +
+                '}';
+    }
 }
